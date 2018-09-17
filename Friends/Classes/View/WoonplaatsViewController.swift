0f49e9f630403868bbf1ffb1cc92e2ba6202e408
Bart den Hollander <bart.den.hollander@ing.nl>
2018-09-17 14:02:17 +0200

    add dummy classes for the woonplaats and favorieten

---
+++ Friends/Classes/View/WoonplaatsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WoonplaatsViewController.swift
+//  Friends
+//
+//  Created by Bart den Hollander on 17/09/2018.
+//  Copyright © 2018 com.bdh.staticlib. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class WoonplaatsViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
